d7d2669fcf54721a74c0c1cc80a21964e700cfda
mersad-rs <mersad.rss@gmail.com>
2024-12-07 10:44:57 +0330

    a little change on home head secion

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Home.css"
-import image1 from "./MP.png"
 
 const Home = () => {
     return(
         <div className="homeContain">
             <div className="homeHead bg-d p-5 mx-5 rounded-5">
-                <img src={image1} alt="wallpaper" className="w-25 rounded-5" />
-                <p className="fs-2">Mersad Haselmehri <br />Desember 29,2004 <br />Programmer<br />Bussines Managment Student</p>
+                <p>Hello and thank you for visiting my personal website! Feel free to browse around, and don’t hesitate to reach out if you have any questions or just want to say hi. Your thoughts and feedback are always welcome!</p>
             </div>
             <div className="w-md-50 container my-5 text-start p-5 rounded-5 bg-d">
                 <p className="des fs-5"><span className="display-5 text-danger fw-bolder">&#60;p&#62;</span>Over the past two years, I have immersed myself in the world of front-end development, transforming my passion for technology and design into tangible skills. Through dedicated learning and hands-on practice, I have gained a solid foundation in creating dynamic and user-friendly web applications.I have become proficient in key technologies such as HTML, CSS, JavaScript, Bootstrap, and React and I am continually expanding my knowledge of frameworks. My journey has allowed me to work on various projects, where I have honed my ability to translate design concepts into functional, responsive websites.I take pride in my attention to detail and my commitment to delivering high-quality work. I am excited to continue growing as a developer and to take on new challenges that push my skills further.<span className="display-5 text-danger fw-bolder">&#60;&#47;p&#62;</span></p>
